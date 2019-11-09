7419a159600eb7889d678e17bec83916b614f604
Shashidhar <shashidhardevraj@gmail.com>
2019-11-09 23:09:14 +0530

    Program to find the leap year

---
+++ find_leap_year.py
@@ -0,0 +1,20 @@
+# Program to find the leap year
+
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+# This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, # 2300 and 2500 are NOT leap years.Source
+
+def is_leap(year):
+    leap = False
+    
+    if year%4 == 0 and year%100 != 0:
+        leap = True
+        return leap
+    if year%400 == 0 and year%100 == 0:
+        leap = True
+        return leap
+    return leap
+
+year = int(input())
+print(is_leap(year))
